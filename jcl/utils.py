b745acceeb950676beae285936a371098ef53022
Predrag Živadinović <predrag.zivadinovic@ist.ac.at>
2021-08-26 15:18:41 +0200

    Small refractoring of utils module and additional documentation for functions.

--- jcl/utils.py
+++ jcl/utils.py
@@ -22,10 +22,9 @@ def align_measured_data(population_vectors, positions, num_bins, speed=None):
     else:
         return pv, pos
 
-
 def __interpolate_linear_position(position, unknown_val=-1, interp_order=1):
     """ Spline interpolation of missing values in animal position data.
-        
+
         Args:
             position - coordinates
             unknown_val - placeholder for unknown/missing values
@@ -37,11 +36,11 @@ def __interpolate_linear_position(position, unknown_val=-1, interp_order=1):
     ok_inds = np.where(position != unknown_val)[0]
     f = InterpolatedUnivariateSpline(ok_inds, position[ok_inds], k=interp_order)
     return f(np.arange(len(position)))
-    
+
 def interpolate_position(x, y=None, unknown_val=-1, interp_order=1):
     """ Spline interpolation of missing values in animal position data.
         In case of 2d data (y not None), each dimension is interpolated independently.
-        
+
         Args:
             x - coordinates
             y - coordinates (for 2d data only)
@@ -52,21 +51,20 @@ def interpolate_position(x, y=None, unknown_val=-1, interp_order=1):
     """
     x_i = __interpolate_linear_position(x, unknown_val, interp_order)
     if y is not None:
-        y_i =  __interpolate_linear_position(y, unknown_val, interp_order)
+        y_i = __interpolate_linear_position(y, unknown_val, interp_order)
         return x_i, y_i
-    
     return x_i
 
-def trial_duration(xy_trajectory, sampling_rate=40):
-    """ Compute trial duration based on given trajectory and sampling rate.
+def trial_duration(xy_trajectory, sampling_rate=39.0625):
+    """ Compute trial duration in seconds based on given trajectory and sampling rate.
 
     Args:
         xy_trajectory - animal trajectory during a trial, array of shape (n,2)
-        sampling_rate - number of samples per second (Hz)
+        sampling_rate - number of samples per second (Hz), default 39.0625
     Return:
-        Time (in seconds)
+        Duration in seconds
     """
-    return len(xy_trajectory) * (1000000 / sampling_rate) / 1000000 # in seconds
+    return len(xy_trajectory) / sampling_rate  # in seconds
 
 def trial_distance(xy_trajectory):
     """ Compute trial duration based on given trajectory.
@@ -76,13 +74,13 @@ def trial_distance(xy_trajectory):
     Return:
         Distance covered by the trajectory (in the same units as xy_trajectory)
     """
-    diffs = xy_trajectory[1:,:] - xy_trajectory[0:-1,:]
+    diffs = np.diff(xy_trajectory, axis=0)
     distances = np.linalg.norm(diffs, axis=1)
     return distances.sum()
 
+# TODO unit tests
 """
 # quick test
-# TODO unit tests
 
 x = [1, 2, -1, 4, 5, 6]
 y = [-1, 2, 3, -1, 5, -1]
