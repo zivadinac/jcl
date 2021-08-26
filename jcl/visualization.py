2a466191e12c9e5b8c4069733554780f613c653d
Predrag Živadinović <predrag.zivadinovic@ist.ac.at>
2021-08-26 15:19:24 +0200

    Small refractoring of visualization module and additional documentation for functions.

--- jcl/visualization.py
+++ jcl/visualization.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors.LinearSegmentedColormap import from_list
 from matplotlib.cm import ScalarMappable
 from jcl.utils import trial_distance
 
@@ -8,10 +8,10 @@ def plot_place_field(place_field):
     """ Plot place field with added color bar."
 
         Args:
-            place_field - place field as np.array
+            place_field - place field as an np.array
     """
     if place_field.ndim == 1:
-        im = np.expand_dims(place_field, 1)   
+        im = np.expand_dims(place_field, 1)
         im = np.repeat(im, 3, 1).T
         plt.yticks([])
         plt.xticks(np.arange(place_field.shape[0]))
@@ -24,7 +24,15 @@ def plot_place_field(place_field):
     plt.colorbar()
     plt.show()
 
-def plot_trajectory(xy_trajectory, size, title=None, sampling_rate=40):
+def plot_trajectory(xy_trajectory, size, title=None, sampling_rate=39.0625):
+    """ Plot given trajectory.
+
+    Args:
+        xy_trajectory - animal trajectory during a trial, array of shape (n,2)
+        size - figure size, tuple
+        title - figure title
+        sampling_rate - number of samples per second (Hz), default 39.0625
+    """
     xy_trajectory = xy_trajectory.astype(np.int16)
     im = np.ones((size[0], size[1], 3))
 
@@ -34,7 +42,7 @@ def plot_trajectory(xy_trajectory, size, title=None, sampling_rate=40):
         im[p[0], p[1]] = color
 
     plt.imshow(im, vmin=0., vmax=1.)
-    cmap = ScalarMappable(cmap=LinearSegmentedColormap.from_list('time_cmap', [(0.,0.,0.), (1.,0.,0.)]))
+    cmap = ScalarMappable(cmap=from_list('time_cmap', [(0., 0., 0.), (1., 0., 0.)]))
     cb = plt.colorbar(cmap, ticks=[0., 1.])
     cb.set_ticklabels([0, np.round(trial_distance(xy_trajectory), 2)])
     cb.set_label("Distance covered", fontsize=18)
