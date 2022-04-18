580b1155a5ac04b5eb54f5eeeb6a8a137d8b5a79
Predrag Živadinović <predrag.zivadinovic@ist.ac.at>
2022-04-18 14:49:04 +0200

    Object oriented Firint rate API.

--- jcl/analysis.py
+++ jcl/analysis.py
@@ -1,122 +1,278 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
+from scipy.spatial.distance import cosine
 
 
-def __calc_num_bins(maze_size, bin_size):
-    """ Calculate number of spatial bins.
+class Binning:
+    @staticmethod
+    def bin_idx(p, bin_size):
+        """ Calculate bin index for given position.
 
-        Args:
-            maze_size - size of the maze
-            bin_size - size of a bin
-        Return:
-            number of bins, same shape as maze_size
-    """
-    return 1 + (np.array(maze_size) / bin_size).astype(int)
+            Args:
+                p - position
+                bin_size - size of bins, single number or same shape as `p`
+            Return:
+                bin index - tuple of the same shape as `p`
+        """
+        return tuple((p // bin_size).astype(int))
 
+    @staticmethod
+    def num_bins(maze_size, bin_size):
+        """ Calculate number of spatial bins.
 
-def __get_bin_idx(p, bin_size):
-    """ Calculate bin index for given position.
-
-        Args:
-            p - position
-            bin_size - size of bins
-        Return:
-            bin index - tuple of the same shape as p
-    """
-    return tuple((p // bin_size).astype(int))
-
-
-def occupancy_map(positions, maze_size, bin_size, bin_len, smooth_sd=None):
-    """ Produce occupancy map.
-
-        Args:
-            positions - array of positions of shape (n,1) or (n,2)
-            maze_size - size of the maze in the same units as positions
-            bin_size - size of spatial bins in the same units as positions
-            bin_len - duration of temporal bins in ms
-            smooth_sd - SD in bins for gaussian smoothing
-        Return:
-            occupancy - time in seconds spent in each spatial bin
-    """
-    occupancy = np.zeros(__calc_num_bins(maze_size, bin_size))
-
-    for p in positions:
-        bin_idx = __get_bin_idx(p, bin_size)
-        occupancy[bin_idx] += bin_len
-
-    if smooth_sd is not None:
-        occupancy = gaussian_filter(occupancy, smooth_sd)
-
-    return occupancy / 1000 # ms to seconds
+            Args:
+                maze_size - size of the maze
+                bin_size - size of a bin
+            Return:
+                number of bins, same shape as maze_size
+        """
+        return 1 + (np.array(maze_size) / bin_size).astype(int)
 
 
-def place_fields(spike_train, positions, maze_size, bin_size, bin_len, smooth_sd=3, return_occupancy=False):
-    """ Produce place field map for the given single cell spike train.
+class Map:
+    def __init__(self, mmap):
+        self.__map = mmap
+        self.__map_prob = None
 
-        Args:
-            positions - array of positions of shape (n,1) or (n,2)
-            maze_size - size of the maze in the same units as positions
-            bin_size - size of spatial bins in the same units as positions
-            bin_len - duration of temporal bins in ms
-            smooth_sd - SD in bins for gaussian smoothing
-            return_occupancy - whether to return occupancy map
-        Return:
-            place field map - matrix with firing rate (Hz) in each spatial bin
-    """
-    assert len(spike_train) == len(positions)
-    occupancy = occupancy_map(positions, maze_size, bin_size, bin_len, smooth_sd)
-    pfs = np.zeros_like(occupancy)
+    @property
+    def map(self):
+        """ Return the underlying map. """
+        return self.__map
 
-    for  p, sn in zip(positions, spike_train):
-        bin_idx = __get_bin_idx(p, bin_size)
-        pfs[bin_idx] += sn
+    @property
+    def map_prob(self):
+        """ Return the underlying map normalized to sum of 1. """
+        if self.__map_prob is None:
+            self.__map_prob = self.__map / self.__map.sum()
+        return self.__map_prob
 
-    pfs = pfs / occupancy
-    pfs[np.isnan(pfs)] = 0
-    pfs[np.isinf(pfs)] = 0
-    pfs = gaussian_filter(pfs, smooth_sd)
-    return pfs, occupancy if return_occupancy else pfs
+    # a map is an array
+    # so we implement methods that are
+    # absolutely essential for an array
+    def __getitem__(self, idx):
+        return self.__map.__getitem__(idx)
 
+    @property
+    def ndim(self):
+        return self.__map.ndim
 
-def frs(place_fields, eps=1e-15):
-    """ Compute mean, median and max firing rates from given place field map.
-        Ignore nans, infs and values smaller than eps.
+    @property
+    def shape(self):
+        return self.__map.shape
 
-        Args:
-            place_fields - map with firing rates for spatial bins
-            eps - precision, ignore bins with values smaller than eps
-        Return:
-            (mean, median, max) FR in Hz
-    """
-    not_nan = np.logical_not(np.isnan(place_fields))
-    not_inf = np.logical_not(np.isinf(place_fields))
-    good = np.logical_and(not_nan, not_inf)
-    good = np.logical_and(good, place_fields >= eps)
-    pfs = place_fields[good]
-    return pfs.mean(), np.median(pfs), pfs.max()
+    @property
+    def size(self):
+        return self.__map.size
 
 
-def place_field_sharpness(place_fields, occupancy, eps=1e-15):
-    """ Compute sharpness of given place field (in bits/spike).
+class OccupancyMap(Map):
+    def __init__(self, positions, maze_size, bin_size, bin_len, smooth_sd=None):
+        """ Produce occupancy map.
 
-        Args:
-            place_fields - map with firing rates for spatial bins
-            occupancy - map with time spent in each spatial bin
-            eps - precision, ignore bins with values smaller than eps
-        Return:
-            sharpness in bits/spike
-    """
-    assert place_fields.shape == occupancy.shape
-    occ_prob = occupancy / occupancy.sum()
-    mfr = frs(place_fields, eps)[0]
+            Args:
+                positions - array of positions of shape (n,1) or (n,2)
+                maze_size - size of the maze in the same units as positions
+                bin_size - size of spatial bins in the same units as positions
+                bin_len - duration of temporal bins in ms
+                smooth_sd - SD in bins for gaussian smoothing
+            Return:
+                occupancy - time in seconds spent in each spatial bin
+        """
+        super().__init__(self.__compute_occ(positions, maze_size, bin_size, bin_len, smooth_sd))
+        self.bin_size = bin_size
 
-    per_bin = []
-    for fr, op in zip(place_fields.flatten(), occ_prob.flatten()):
-        if np.isnan(fr) or np.isnan(op) or\
-           np.isinf(fr) or np.isinf(op) or\
-           fr < eps or op < eps:
-            continue
-        i = fr / mfr
-        per_bin.append(i * np.log2(i) * op)
-    return np.sum(per_bin)
-    
+    @staticmethod
+    def __compute_occ(positions, maze_size, bin_size, bin_len, smooth_sd=None):
+        occupancy = np.zeros(Binning.num_bins(maze_size, bin_size))
+
+        for p in positions:
+            bin_idx = Binning.bin_idx(p, bin_size)
+            occupancy[bin_idx] += bin_len
+
+        if smooth_sd is not None:
+            occupancy = gaussian_filter(occupancy, smooth_sd)
+
+        return occupancy / 1000 # ms to seconds
+
+
+class FiringRateMap(Map):
+    def __init__(self, spike_train, positions, maze_size, bin_size, bin_len, smooth_sd=3):
+        """ Produce firing rate map for the given single cell spike train.
+
+            Args:
+                positions - array of positions of shape (n,1) or (n,2)
+                maze_size - size of the maze in the same units as positions
+                bin_size - size of spatial bins in the same units as positions
+                bin_len - duration of temporal bins in ms
+                smooth_sd - SD in bins for gaussian smoothing
+            """
+        __fr_map, __occupancy = self.__compute_frm(spike_train, positions, maze_size, bin_size, bin_len, smooth_sd=3, return_occupancy=True)
+        super().__init__(__fr_map)
+        self.__occupancy = __occupancy
+        self.bin_size = bin_size
+        self.__eps = 1e-15
+        self.__I_sec = None
+        self.__I_spike = None
+        self.__sparsity = None
+        self.__frs = None # mean, median, max
+
+    @property
+    def occupancy(self):
+        """ Occupancy map. """
+        return self.__occupancy
+
+    @property
+    def peak_fr(self):
+        """ Peak firing rate in Hz. """
+        if self.__frs is None:
+            self.__frs = self.__compute_frs()
+        return self.__frs[2]
+
+    @property
+    def mean_fr(self):
+        """ Mean firing rate in Hz. """
+        if self.__frs is None:
+            self.__frs = self.__compute_frs()
+        return self.__frs[0]
+
+    @property
+    def I_sec(self):
+        """ Information of the firing rate map in bits/s. """
+        if self.__I_sec is None:
+
+            good = np.logical_and(self.__good_idx(self.map, self.__eps), self.__good_idx(self.__occupancy.map, self.__eps))
+            frm = self.map[good]
+
+            occ = self.__occupancy.map_prob[good]
+
+            self.__I_sec = np.sum(frm * np.log2(frm / self.mean_fr) * occ)
+        return self.__I_sec
+
+
+    @property
+    def I_spike(self):
+        """ Information of the firing rate map in bits/spike. """
+        if self.__I_spike is None:
+            self.__I_spike = self.I_sec / self.mean_fr
+        return self.__I_spike
+
+    @property
+    def sparsity(self):
+        """ Sparsity of the firing rate map. """
+        if self.__sparsity is None:
+            occ_prob = self.__occupancy.map_prob
+            frm_good_idx = self.__good_idx(self.map)
+            occ_good_idx = self.__good_idx(occ_prob)
+            good_idx = np.logical_and(frm_good_idx, occ_good_idx)
+
+            frm = self.map[good_idx]
+            occ = occ_prob[good_idx]
+
+            self.__sparsity = np.sum(frm * occ) ** 2 / np.sum(frm ** 2 * occ)
+        return self.__sparsity
+
+    def __compute_frs(self):
+        """ Compute mean, median and max firing rates from given firing rate map.
+            Ignore nans, infs and values smaller than eps.
+
+            Return:
+                (mean, median, max) FR in Hz
+        """
+        not_nan = np.logical_not(np.isnan(self.map))
+        not_inf = np.logical_not(np.isinf(self.map))
+        good = np.logical_and(not_nan, not_inf)
+        good = np.logical_and(good, self.map >= self.__eps)
+        frm = self.map[good]
+        if frm.size == 0:
+            return 0, 0, 0
+        return frm.mean(), np.median(frm), frm.max()
+
+    @staticmethod
+    def __compute_frm(spike_train, positions, maze_size, bin_size, bin_len, smooth_sd=3, return_occupancy=False):
+        """ Produce firing rate map for the given single cell spike train.
+
+            Args:
+                positions - array of positions of shape (n,1) or (n,2)
+                maze_size - size of the maze in the same units as positions
+                bin_size - size of spatial bins in the same units as positions
+                bin_len - duration of temporal bins in ms
+                smooth_sd - SD in bins for gaussian smoothing
+                return_occupancy - whether to return occupancy map
+            Return:
+                firing rate map - matrix with firing rate (Hz) in each spatial bin
+        """
+        assert len(spike_train) == len(positions)
+        occupancy = OccupancyMap(positions, maze_size, bin_size, bin_len, smooth_sd)
+        frm = np.zeros_like(occupancy.map)
+
+        for  p, sn in zip(positions, spike_train):
+            bin_idx = Binning.bin_idx(p, bin_size)
+            frm[bin_idx] += sn
+
+        frm = frm / occupancy.map
+        frm[np.isnan(frm)] = 0
+        frm[np.isinf(frm)] = 0
+        frm = gaussian_filter(frm, smooth_sd)
+        return frm, occupancy if return_occupancy else frm
+
+    @staticmethod
+    def __good_idx(m, eps=None):
+        """ Get indices of elements that are not Nan, Inf and that are >= `eps`. """
+        not_nan = np.logical_not(np.isnan(m))
+        not_inf = np.logical_not(np.isinf(m))
+        good = np.logical_and(not_nan, not_inf)
+        if eps is not None:
+            return np.logical_and(good, m >= eps)
+        else:
+            return good
+
+
+class PopulationVectors:
+    def __init__(self, fr_maps):
+        """ Get population vectors from provided firing rate maps.
+
+            Args:
+                fr_maps - list of FiringRateMap, all of the same shape
+            Return:
+                population vectors - np.array of shape (*(fr_maps[0].shape), len(fr_maps))
+        """
+        self.pvs = np.stack([frm.map for frm in fr_maps], axis=-1)
+
+    @property
+    def spatial_shape(self):
+        return self.pvs.shape[:-1]
+
+    def measure(self, other, measure_fun):
+        """ Compute measure `measure_fun` between all pairs of population vectors.
+
+            Args:
+                other - other population vectors
+                measure_fun - function that accepts two vectors and returns a number
+            Return:
+                list of measure values
+        """
+        assert self.pvs.shape == other.pvs.shape
+        spatial_shape = self.pvs.shape[:-1]
+        measure = np.array([measure_fun(self.pvs[idx], other.pvs[idx]) for idx in np.ndindex(spatial_shape)])
+        return measure[~np.isnan(measure)]
+
+    def correlation(self, other):
+        """ Compute Pearson correlation coefficients between all pairs of population vectors.
+
+            Args:
+                other - other population vectors
+            Return:
+                correlation coefficients - list of correlation coefficients
+        """
+        return self.measure(other, lambda pv1, pv2: np.corrcoef(pv1, pv2)[0,1])
+
+    def cosine_distance(self, other):
+        """ Compute correlation coefficients between all pairs of population vectors.
+
+            Args:
+                other - other population vectors
+            Return:
+                list of cosine distances
+        """
+        return self.measure(other, cosine)
+
--- jcl/visualization.py
+++ jcl/visualization.py
@@ -4,55 +4,84 @@ from matplotlib.colors import LinearSegmentedColormap
 from_list = LinearSegmentedColormap.from_list
 from matplotlib.cm import ScalarMappable
 from jcl.utils import trial_distance, trial_duration
+from jcl.analysis import Map
 
-def plot_place_field(place_field, title=None):
-    """ Plot place field with added color bar."
+
+def plot_map(m: Map, title=None, colorbar_label=None, path=None):
+    """ Plot given with added color bar."
 
         Args:
-            place_field - place field as an np.array
+            m - map
+            title - figure title
+            colorbar_label - label to be used for the colorbar
+            path - file path to which to save the figure, if none show the figure
     """
-    if place_field.ndim == 1:
-        im = np.expand_dims(place_field, 1)
-        im = np.repeat(im, 3, 1).T
+    if m.ndim == 1:
+        im = np.expand_dims(m.map, 1)
+        im = np.repeat(im.map, 3, 1).T
     else:
-        im = place_field
+        im = m.map
 
     plt.set_cmap("jet")
     plt.imshow(im)
-    plt.colorbar()
+
+    cb = plt.colorbar(ticks=[0., np.max(im)])
+    if colorbar_label is not None:
+        cb.set_ticklabels([0, np.round(np.max(im))])
+        cb.set_label("Hz", fontsize=18)
+
     if title is not None:
         plt.title(title)
-    plt.show()
 
-def plot_trajectory(xy_trajectory, size, sampling_rate, title=None):
+    if path is not None:
+        plt.savefig(path)
+    else:
+        plt.show()
+    plt.close()
+
+
+def plot_trajectory(trajectory, size, sampling_rate, title=None, path=None, trajectory_2=None):
     """ Plot given trajectory.
 
-    Args:
-        xy_trajectory - animal trajectory during a trial, array of shape (n,2)
-        size - figure size, tuple
-        sampling_rate - number of samples per second (Hz)
-        title - figure title
+        Args:
+            trajectory - animal trajectory during a trial, array of shape (n,2)
+            size - figure size, tuple
+            sampling_rate - number of samples per second (Hz)
+            title - figure title
+            path - file path to which to save the figure, if none show the figure
+            trajectory_2 - second animal trajectory during a trial, array of the same shape as trajectory
     """
-    xy_trajectory = xy_trajectory.astype(np.int16)
     im = np.ones((size[0]+1, size[1]+1, 3))
 
-    for i in range(len(xy_trajectory)):
-        cc = i / len(xy_trajectory)
+    trajectory = trajectory.astype(np.int16)
+    for i in range(len(trajectory)):
+        cc = i / len(trajectory)
         color = (cc, 0., cc)
-        #p = [xy_trajectory[i][1], xy_trajectory[i][0]]
-        p = [xy_trajectory[i][0], xy_trajectory[i][1]]
+        p = [trajectory[i][0], trajectory[i][1]]
         im[p[0], p[1]] = color
+
+    if trajectory_2 is not None:
+        trajectory_2 = trajectory_2.astype(np.int16)
+        for i in range(len(trajectory_2)):
+            p = [trajectory_2[i][0], trajectory_2[i][1]]
+            im[p[0], p[1]] = (0., 1., 0.)
 
     plt.imshow(im, vmin=0., vmax=1.)
     plt.xticks([])
     plt.yticks([])
     cmap = ScalarMappable(cmap=from_list('time_cmap', [(0., 0., 0.), (1., 0., 1.)]))
     cb = plt.colorbar(cmap, ticks=[0., 1.])
-    cb.set_ticklabels([0, np.round(trial_duration(xy_trajectory, sampling_rate), 2)])
+    cb.set_ticklabels([0, np.round(trial_duration(trajectory, sampling_rate), 2)])
     cb.set_label("Time (s)", fontsize=18)
+
     if title is not None:
         plt.title(title, fontsize=24)
-    plt.show()
+
+    if path is not None:
+        plt.savefig(path)
+    else:
+        plt.show()
+    plt.close()
 
 
 """
